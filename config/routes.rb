aed1740b26e1f9510c1545dc89b0d0cca55946cb
msoler18 <msoler735@gmail.com>
2018-05-20 21:51:52 -0500

    Add Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create, :destroy]
   end
+
+  get '/about', to: 'pages#about'
 end
